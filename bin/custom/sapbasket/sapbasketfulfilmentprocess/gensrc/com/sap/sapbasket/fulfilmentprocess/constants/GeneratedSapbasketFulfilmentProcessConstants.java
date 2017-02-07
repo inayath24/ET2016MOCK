1e60c468460e7e5981938411a974fd22410389e0
Akash Agarwal <akash.agarwal01@sap.com>
2017-02-07 12:19:10 +0530

    regsiter done

--- bin/custom/sapbasket/sapbasketfulfilmentprocess/gensrc/com/sap/sapbasket/fulfilmentprocess/constants/GeneratedSapbasketFulfilmentProcessConstants.java
+++ bin/custom/sapbasket/sapbasketfulfilmentprocess/gensrc/com/sap/sapbasket/fulfilmentprocess/constants/GeneratedSapbasketFulfilmentProcessConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Feb 3, 2017 10:44:46 PM                     ---
+ * --- Generated at Feb 4, 2017 3:19:54 PM                      ---
  * ----------------------------------------------------------------
  */
 package com.sap.sapbasket.fulfilmentprocess.constants;
